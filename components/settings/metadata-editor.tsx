e61ed67e8db0b620f66324b567d06a22df7c3fbb
Salik <iamsalikjamal@gmail.com>
2024-05-17 00:35:59 +0500

    Image upload with form

--- components/settings/metadata-editor.tsx
+++ components/settings/metadata-editor.tsx
@@ -1,7 +1,97 @@
+import { z } from "zod"
+import { useState } from "react"
+import toast from "react-hot-toast"
+import { useForm } from "react-hook-form"
+import { zodResolver } from "@hookform/resolvers/zod"
+import { Button } from "@/components/ui/button"
+import { Input } from "@/components/ui/input"
+import { 
+    Form, 
+    FormControl, 
+    FormField, 
+    FormItem, 
+    FormLabel, 
+    FormMessage
+} from "@/components/ui/form"
+import FaviconUpload from "@/components/ui/favicon-upload"
+
+
+type MetadataFormValues = z.infer<typeof formSchema>
+const formSchema = z.object({
+    label: z.string().min(1, { message: "Name is required" }),
+    imageURL: z.string().min(1, { message: "Favicon is required" })
+})
+
+
 export default function MetadataEditor() {
+
+    const [loading, setLoading] = useState(false)
+
+    const form = useForm<MetadataFormValues>({
+        resolver: zodResolver(formSchema),
+        defaultValues: {
+            label: "",
+            imageURL: ""
+        }
+    })
+
+    const onSubmit = async (values: MetadataFormValues) => {
+        try {
+            console.log(values)
+            toast.success("SUCCESS.")
+        } catch(err) {
+            toast.error("Something went wrong.")
+        } finally {
+            setLoading(false)
+        }
+    }
+
     return (
-        <div className="px-5">
-            METADATA EDITOR
+        <div className="flex flex-col gap-14 px-5">
+            <Form {...form}>
+                <form className="space-y-8 w-full" onSubmit={form.handleSubmit(onSubmit)}>
+                    <FormField 
+                        control={form.control}
+                        name="imageURL"
+                        render={({ field }) => (
+                            <FormItem>
+                                <FormLabel>Favicon</FormLabel>
+                                <FormControl>
+                                    <FaviconUpload 
+                                        value={field.value}
+                                        disabled={loading}
+                                        onChange={value => field.onChange(value)}
+                                    />
+                                </FormControl>
+                                <FormMessage />
+                            </FormItem>
+                        )}
+                    />
+                    <FormField 
+                        control={form.control}
+                        name="label"
+                        render={({ field }) => (
+                            <FormItem>
+                                <FormLabel className="text-stone-400">Page title</FormLabel>
+                                <FormControl>
+                                    <Input
+                                        placeholder="Billboard label"
+                                        disabled={loading}
+                                        {...field}
+                                    />
+                                </FormControl>
+                                <FormMessage />
+                            </FormItem>
+                        )}
+                    />
+                    <Button 
+                        type="submit" 
+                        disabled={loading}
+                    >
+                        Submit
+                    </Button>
+                </form>
+            </Form>
         </div>
     )
 }
---
+++ components/ui/favicon-upload.tsx
@@ -0,0 +1,55 @@
+import { ChangeEvent, useState } from "react"
+import Image from "next/image"
+import { Button } from "@/components/ui/button"
+
+interface IFaviconUploadProps {
+    value: string;
+    onChange: (value: string) => void;
+    disabled?: boolean;
+}
+
+export default function FaviconUpload({ value, onChange, disabled }: IFaviconUploadProps) {
+    
+    const handleImageChange = (e: ChangeEvent<HTMLInputElement>) => {
+
+        const file = e.target.files?.[0]
+
+        if(file) {
+            const reader = new FileReader()
+            reader.onload = () => {
+                onChange(reader.result as string)
+            }
+            reader.readAsDataURL(file)
+        }
+    }
+    
+    return (
+        <div className="flex gap-4 items-center">
+            <Image 
+                className="size-[75px] rounded-md object-cover"
+                sizes="100%"
+                quality={100}
+                src={value || "https://placehold.co/75x75/png"}
+                alt="favicon"
+                width={75}
+                height={75}
+            />
+            <div className="relative">
+                <Button 
+                    className="text-sm rounded-full w-auto bg-gray-100 cursor-pointer text-black" 
+                    type="button"
+                    variant="ghost"
+                    disabled={disabled}
+                >
+                    Upload favicon
+                    <input
+                        className="absolute top-0 left-2.5 opacity-0 w-28 h-10 cursor-pointer"
+                        type="file"
+                        accept="image/*"
+                        onChange={handleImageChange}
+                    />
+                </Button>
+            </div>
+        </div>
+    )
+}
