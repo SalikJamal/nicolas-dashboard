cf189fcaa859e2b90fc4d515b21d3c015eaae0ee
Salik <iamsalikjamal@gmail.com>
2024-05-17 16:09:12 +0500

    Fixed height issue

--- app/settings/layout.tsx
+++ app/settings/layout.tsx
@@ -3,23 +3,18 @@ import RightPanel from "@/components/settings/right-panel"
 import Header from "@/components/ui/header"
 import { IReactChildren } from "@/lib/types"
 
-interface ISettingsLayoutProps extends IReactChildren {
-    leftPanel: React.ReactNode;
-    rightPanel: React.ReactNode;
-}
 
-
-export default function SettingsLayout({ children, leftPanel, rightPanel }: ISettingsLayoutProps) {
+export default function SettingsLayout({ children }: IReactChildren) {
     return (
-        <div>
-                <Header title="Settings" />
-                <div className="flex flex-col sm:flex-row justify-between">
-                    <LeftPanel />
-                    <div className="flex justify-center md:mt-40">
-                        {children}
-                    </div>
-                    <RightPanel />
-                </div>
-        </div>
+        <>
+            <Header title="Settings" />
+            <div className="flex flex-col min-h-[calc(100%-5rem)] sm:flex-row justify-between">
+                <LeftPanel />
+                <main className="flex justify-center md:mt-40">
+                    {children}
+                </main>
+                <RightPanel />
+            </div>
+        </>
     )
 }
